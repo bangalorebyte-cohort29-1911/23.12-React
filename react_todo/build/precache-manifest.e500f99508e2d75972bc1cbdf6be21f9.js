0e6d19b2f33bb2d852fa4e5e1526fd646fa76495
datapsyche <jithinjkumar@gmail.com>
2019-12-27 11:24:41 +0530

    added react todo

---
+++ react_todo/build/precache-manifest.e500f99508e2d75972bc1cbdf6be21f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d21b2b583647706c712e7bc0b28e46a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "747bd472a7c88dda90df",
+    "url": "/static/css/main.16260e3f.chunk.css"
+  },
+  {
+    "revision": "874408fef922eec4ba29",
+    "url": "/static/js/2.a7dd29fc.chunk.js"
+  },
+  {
+    "revision": "747bd472a7c88dda90df",
+    "url": "/static/js/main.a48f4d99.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
